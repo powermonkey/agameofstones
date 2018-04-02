3f63d380df04dd995996f6c47e7c56564f561ca6
Rodolfo Cam II <rodiicam@gmail.com>
2018-04-03 00:09:28 +0800

    LibGdx create project.

---
+++ desktop/src/com/agameofstones/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.agameofstones.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.agameofstones.game.AGameOfStones;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AGameOfStones(), config);
+	}
+}
---
+++ html/src/com/agameofstones/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.agameofstones.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.agameofstones.game.AGameOfStones;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new AGameOfStones();
+        }
+}
